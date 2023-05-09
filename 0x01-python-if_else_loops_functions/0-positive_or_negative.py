68488e838446d22b66f8ba137ebb7cfcf7607147
alantiren <tirenalan0@gmail.com>
2023-05-09 20:57:04 +0300

    update

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10, 10)  # generates a random integer between -10 and 10, inclusive
-
 print(number)
-
-if number > 0:
-    print("is positive")
-elif number == 0:
-    print("is zero")
+if
+number > 0:
+print("is positive")
+elif
+number == 0:
+print("is zero")
 else:
-    print("is negative")
-    
+print("is negative")  
 print()  # prints a newline character
