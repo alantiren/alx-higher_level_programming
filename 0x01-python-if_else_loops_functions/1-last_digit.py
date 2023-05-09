b8cfbafa30bb5d0defe99abd2bea5056cfa8b4d7
alantiren <tirenalan0@gmail.com>
2023-05-09 21:51:18 +0300

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)  # generates a random integer between -10000 and 10000, inclusive
+number = random.randint(-10000, 10000)
 
 print("Last digit of", number, "is", abs(number) % 10, end=" ")
 digit = abs(number) % 10
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-for l in range(97, 123):
-    print("{}".format(chr(l)), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
