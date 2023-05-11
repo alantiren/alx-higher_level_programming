064b3114de1d499352a3b5092f1b9f584257e90e
alantiren <122862761+alantiren@users.noreply.github.com>
2023-05-11 21:31:10 +0300

    updates

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,6 @@ from calculator_1 import add, sub, mul, div
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
-
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-operator = sys.argv[2]
-
 if operator == "+":
     print("{} + {} = {}".format(a, b, add(a, b)))
 elif operator == "-":
@@ -21,3 +16,8 @@ elif operator == "/":
 else:
     print("Unknown operator. Available operators: +, -, * and /")
     sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+operator = sys.argv[2]
+print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
