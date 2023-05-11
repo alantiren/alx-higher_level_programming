081ac4c13a824f9477e914b192efe689b992c972
alantiren <122862761+alantiren@users.noreply.github.com>
2023-05-11 22:17:57 +0300

    updates

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
 from calculator_1 import add, sub, mul, div
 
 if len(sys.argv) != 3:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    res = 0
-    for i in range(len(sys.argv) - 1):
-        res += int(sys.argv[i + 1])
-    print("{}".format(res))
 
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
