8b0a50ed1475f67f587a73eb9745a83c7a5831fb
alantiren <122862761+alantiren@users.noreply.github.com>
2023-05-11 21:49:44 +0300

    updates

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     c = 0
     if a < b:
         add, sub = __import__('magic_calculation_102').add, __import__('magic_calculation_102').sub
