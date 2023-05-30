d3e828182db553f99b68356de3fca8b757c8e27d
alantiren <tirenalan0@gmail.com>
2023-05-30 22:00:12 +0300

    updates

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-"""
-Module -MagicClass
-MagicClass matching exactly a bytecode provided by Holberton.
-"""
+"""Module -MagicClass
+MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
 
@@ -26,9 +23,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self) -> float:
-        """Calculate the area of the circle."""
+        """Return Calculated the area of the circle."""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self) -> float:
-        """Calculate the circumference of the circle."""
+        """Return Calculated the circumference of the circle."""
         return(2 * math.pi * self.__radius)
