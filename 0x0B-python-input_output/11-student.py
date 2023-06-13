64190e5c0376d2522bb5ce1cf7f5cd39dbeeea10
alantiren <tirenalan0@gmail.com>
2023-06-13 19:50:39 +0300

    updates

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,7 +7,6 @@
 class Student:
     """Represents a student."""
 
-
     def __init__(self, first_name, last_name, age):
         """Initializes a new Student instance.
 
@@ -19,7 +18,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance.
