25f1351fa09ba61a4365bfe9a8bda0ac036d9382
alantiren <tirenalan0@gmail.com>
2023-06-13 21:57:02 +0300

    updates

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # 0-lookup.py
-"""Defines a function to lookup attributes and methods of an object"""
-
 
 def lookup(obj):
     """Returns a list of available attributes and methods of an object"""
-    return [attribute for attribute in dir(obj)
-            if not attribute.startswith('__')]
+    attributes = []
+    for attribute in dir(obj):
+        if not attribute.startswith('__'):
+            attributes.append(attribute)
+    return attributes
