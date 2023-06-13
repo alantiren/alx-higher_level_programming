de29c9e538cf20745b1c8bbe283f6e81fe9171b5
alantiren <tirenalan0@gmail.com>
2023-06-13 18:32:29 +0300

    updates

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
