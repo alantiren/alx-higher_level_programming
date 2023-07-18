5c2a629a32d67bbd837270638d408f0d87f2c5dc
alantiren <tirenalan0@gmail.com>
2023-07-18 21:01:24 +0300

    updates

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,6 @@
 -- Script: 7-insert_value.sql
 -- Task: Insert a new row into the table first_table in the database hbtn_0c_0.
 -- This SQL query will insert a new row into the table first_table.
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
