fc68415d3378298ac144cbe830908a8364345ac4
alantiren <tirenalan0@gmail.com>
2023-07-18 20:55:29 +0300

    UPDATES

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,8 +1,4 @@
 -- Script: 3-list_tables.sql
 -- Task: List all tables of a specific database in your MySQL server.
-
--- Replace 'your_database_name' with the name of the database you want to list tables for.
-USE your_database_name;
-
 -- This SQL query will list all the tables in the specified database.
 SHOW TABLES;
--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,8 +1,4 @@
 -- Script: 6-list_values.sql
 -- Task: List all rows of the table first_table from the database hbtn_0c_0.
-
--- Replace 'your_database_name' with the name of the database.
-USE your_database_name;
-
 -- This SQL query will list all rows of the table first_table.
 SELECT * FROM first_table;
