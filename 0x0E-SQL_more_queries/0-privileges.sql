335fa5529951d31f43b61d5519a4e9d69c2241f7
alantiren <tirenalan0@gmail.com>
2023-07-19 20:01:07 +0300

    updates

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- 0-privileges.sql
--- List privileges for user_0d_1
+-- List privileges for user_0d_1 and for user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
