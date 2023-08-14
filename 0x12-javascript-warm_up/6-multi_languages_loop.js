d534c1d06c14fe9660884504c149011089c08a2b
alantiren <tirenalan0@gmail.com>
2023-08-14 22:22:53 +0300

    updates

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,6 @@
 // Define an array of strings
 // Loop through the array and print each line
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
