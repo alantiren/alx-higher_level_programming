905aa11db5b9125d05df79dba579dcf1f1781640
alantiren <tirenalan0@gmail.com>
2023-08-14 22:26:04 +0300

    updates

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Define an array of strings
 // Loop through the array and print each line
-
 for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
   console.log(line);
 }
