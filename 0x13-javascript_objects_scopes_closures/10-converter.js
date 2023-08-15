158a5557313024264386c02597b6d8d822c69727
alantiren <tirenalan0@gmail.com>
2023-08-15 22:55:30 +0300

    updates

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,7 +3,7 @@
 // convert a number from base 10 to another base passed as an argument.
 
 exports.converter = function (base) {
- return function (num) {
-  return num.toString(base);
- };
-};
+    return function (num) {
+      return num.toString(base);
+    };
+  };
