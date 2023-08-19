3c164e725f1e7c1b40e1caa51dc87959e2623f32
alantiren <tirenalan0@gmail.com>
2023-08-19 12:16:25 +0300

    updates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 """
-    A script that lists all states from the database hbtn_0e_0_usa
-    Username, password and database names are given as user args
+Script that lists all states from the database hbtn_0e_0_usa.
 """
-
 
 import sys
 import MySQLdb
-
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-'''Prints all rows in the states table of a database with
-a name starting with 'N'.
-'''
+
+"""
+Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+"""
+
 import sys
 import MySQLdb
 
-
 if __name__ == '__main__':
-    if len(sys.argv) >= 4:
-        db_connection = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-        )
-        cursor = db_connection.cursor()
-        cursor.execute(
-            'SELECT * FROM states WHERE name IS NOT NULL AND' +
-            ' LEFT(CAST(name AS BINARY), 1) = "N" ORDER BY states.id ASC;'
-        )
-        results = cursor.fetchall()
-        for result in results:
-            print(result)
-        db_connection.close()
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
