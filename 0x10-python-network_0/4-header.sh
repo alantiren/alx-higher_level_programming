e723d8960f63ab9b0a05369991cb43648c1b8324
alantiren <tirenalan0@gmail.com>
2023-08-31 14:36:53 +0300

    updates

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Takes a URL, sends a GET request with a specific header, and displays the body of the response
+# Takes a URL, sends a GET request with a specific header,
+# and displays the body of the response
 curl -s "$1" -H "X-School-User-Id: 98
