a03176555f8e5214efc378820392a6c7a8f65e76
alantiren <tirenalan0@gmail.com>
2023-08-31 14:47:24 +0300

    tasks

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with the contents of a file as the request body
-# Displays the body of the response
 curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
