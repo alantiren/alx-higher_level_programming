649039fb9323a42e26e124da9da8f46c883398c3
alantiren <tirenalan0@gmail.com>
2023-09-01 14:38:01 +0300

    tasks

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     body = response.text
-    print(response.text)
     if response.status_code >= 400:
         print("Error code:", response.status_code)
     else:
