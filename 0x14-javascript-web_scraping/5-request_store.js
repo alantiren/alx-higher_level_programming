4c7815d76368d67245b937994e8fd6296a88d502
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-26 16:15:47 +0300

    tasks

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,27 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-if (process.argv.length !== 4) {
-  console.error('Usage: ./5-request_store.js <URL> <file_path>');
-  process.exit(1);
-}
-const url = process.argv[2];
-const filePath = process.argv[3];
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-  if (response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      console.log(`Webpage content has been saved to ${filePath}`);
-    });
-  } else {
-    console.error(`Failed to fetch the webpage. Status code: ${response.statusCode}`);
-    process.exit(1);
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
