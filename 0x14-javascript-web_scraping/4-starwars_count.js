164588aa561527ed2b3189de32e657d7b914bc18
alantiren <122862761+alantiren@users.noreply.github.com>
2023-09-26 16:04:29 +0300

    Revert "tasks"
    This reverts commit e0da964cbaccd7cfe2356dd042b1f0cab08c01f0.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-    if (!error) {
-      const results = JSON.parse(body).results;
-      console.log(results.reduce((count, movie) => {
-        return movie.characters.find((character) => character.endsWith('/18/'))
-          ? count + 1
-          : count;
-      }, 0));
+if (process.argv.length !== 3) {
+  console.error('Usage: ./4-starwars_count.js <api_url>');
+  process.exit(1);
+}
+const apiUrl = process.argv[2];
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  const data = JSON.parse(body);
+  let count = 0;
+  data.results.forEach((movie) => {
+    if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
     }
   });
+  console.log(count);
+});
